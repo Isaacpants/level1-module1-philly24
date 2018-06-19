37bddcf1bc3a4ec766abed449da92cd71f9003ff
League Student <league@iMac-6.attlocal.net>
2018-06-19 10:54:34 -0700

    cat catrunner HarryPotter Harryrunner fin

---
+++ src/HarryRunner.java
@@ -0,0 +1,12 @@
+
+
+public class HarryRunner {
+public static void main(String[] args) {
+	HarryPotter hp = new HarryPotter();
+	
+	hp.makeInvisible(true);
+	hp.spyOnSnape();
+
+	hp.castSpell("Stupefy");
+}
+}
