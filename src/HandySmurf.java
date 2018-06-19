62ace97dd474c92ab00943600477cc806209997a
League Student <league@iMac-6.attlocal.net>
2018-06-19 11:21:34 -0700

    smurf sea creature fin

---
+++ src/HandySmurf.java
@@ -0,0 +1,13 @@
+
+public class HandySmurf {
+public static void main(String[] args) {
+	Smurf hs = new Smurf("HandySmurf");
+	Smurf ps = new Smurf("PapaSmurf");
+	Smurf s = new Smurf("Smurfette");
+	hs.eat();
+	ps.getHatColor();
+	ps.isGirlOrBoy();
+	s.getHatColor();
+	s.isGirlOrBoy();
+}
+}
